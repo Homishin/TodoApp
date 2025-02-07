5458de212e9e09d137084d37d8ef8a81903d3b4e
Homishin <homieshin@gmail.com>
2025-02-08 00:11:14 +0500

    dependency injection jpa

--- src/main/java/com/xomishin/todoApp/controllers/TodoController.java
+++ src/main/java/com/xomishin/todoApp/controllers/TodoController.java
@@ -1,14 +1,34 @@
 package com.xomishin.todoApp.controllers;
 
+import com.xomishin.todoApp.model.ToDoItem;
+import com.xomishin.todoApp.repositories.ToDoItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class TodoController {
+public class TodoController implements CommandLineRunner {
+
+    private final ToDoItemRepository toDoItemRepository;
+
+    public TodoController(ToDoItemRepository toDoItemRepository) {
+        this.toDoItemRepository = toDoItemRepository;
+    }
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        List<ToDoItem> allTodos = toDoItemRepository.findAll();
+        model.addAttribute("allTodos",allTodos);
         return "index";
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        toDoItemRepository.save(new ToDoItem("Уборка"));
+        toDoItemRepository.save(new ToDoItem("Прогулка"));
+    }
 }
---
+++ src/main/java/com/xomishin/todoApp/model/ToDoItem.java
@@ -0,0 +1,38 @@
+package com.xomishin.todoApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ToDoItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+
+    public ToDoItem(){
+
+    }
+
+    public ToDoItem(String title){
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
