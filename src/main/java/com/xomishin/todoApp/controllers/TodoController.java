1b6bd16b1ca7bcfd69c7f2f9e879b98d1e1a5780
Homishin <homieshin@gmail.com>
2025-02-08 14:04:17 +0500

    Base of ToDo App

--- src/main/java/com/xomishin/todoApp/controllers/TodoController.java
+++ src/main/java/com/xomishin/todoApp/controllers/TodoController.java
@@ -5,9 +5,11 @@ import com.xomishin.todoApp.repositories.ToDoItemRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class TodoController implements CommandLineRunner {
@@ -23,7 +25,45 @@ public class TodoController implements CommandLineRunner {
 
         List<ToDoItem> allTodos = toDoItemRepository.findAll();
         model.addAttribute("allTodos",allTodos);
+        model.addAttribute("newTodo", new ToDoItem());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute ToDoItem toDoItem){
+        toDoItemRepository.save(toDoItem);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id){
+        toDoItemRepository.deleteById(id);
+        return "redirect:/";
+    }
+    @PostMapping("/removeAll")
+    public String revomeAllItems(){
+        toDoItemRepository.deleteAll();
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String serchTodoItem(@RequestParam("serchTerm") String serchTerm, Model model) {
+        List<ToDoItem> allTodo = toDoItemRepository.findAll();
+        List<ToDoItem> serchResult = new ArrayList<>();
+
+        for(ToDoItem item : allTodo) {
+            if (item.getTitle().toLowerCase().contains(serchTerm.toLowerCase())) {
+                serchResult.add(item);
+            }
+        }
+
+        model.addAttribute("allTodos",serchResult);
+        model.addAttribute("newTodo", new ToDoItem());
+        model.addAttribute("serchTerm", serchTerm);
+        return "index";
+
     }
 
     @Override
